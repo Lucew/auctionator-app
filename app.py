052d941ce2fcbe32ede66c015f44382785ce3a22
Lucas <weber-lucas@web.de>
2024-12-06 23:37:14 +0100

    finalize ip logging

--- app.py
+++ app.py
@@ -13,25 +13,15 @@ from pandas.api.types import (
     is_object_dtype,
 )
 import urllib.parse
-from streamlit import runtime
-from streamlit.runtime.scriptrunner import get_script_run_ctx
 import logging
 
 
 def get_remote_ip() -> [str|None]:
     """Get remote ip."""
-
-    try:
-        ctx = get_script_run_ctx()
-        if ctx is None:
-            return None
-
-        session_info = runtime.get_instance().get_client(ctx.session_id)
-        if session_info is None:
-            return None
-    except Exception as e:
-        return None
-    return session_info.request.remote_ip
+    # https://github.com/NginxProxyManager/nginx-proxy-manager/issues/674#issuecomment-717459734 for Nginx Proxy Manager
+    # https://docs.streamlit.io/develop/api-reference/utilities/st.context for headers
+    # print(list(st.context.headers.keys()))
+    return st.context.headers.get('X-Real-Ip', 'IP Unknown (not forwarded)')
 
 
 class ContextFilter(logging.Filter):
@@ -143,8 +133,6 @@ st.set_page_config(layout="wide")
 # get the logger
 init_logging()
 __logger = logging.getLogger('auctionator')
-__logger.info(list(st.context.headers.keys()))
-__logger.info(st.context.headers['X-Real-Ip'])
 
 # make a cached function to get the data
 @st.cache_data
